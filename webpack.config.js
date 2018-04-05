7c0ac98b91c79e79b53f6a36b550308d7473eac2
Vijay Kumar <vijay.kasuladevi@ge.com>
2018-04-05 22:17:08 +0530

    basic config

---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+// Webpack configuration
+const path = require('path');
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        path: path.join(__dirname, 'public'),
+        filename: 'bundle.js'
+    },
+    mode: 'development'
+};

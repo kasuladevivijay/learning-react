188d521894d368cd2df50f0114fb7b3293ec3ef5
Vijay Kumar <vijay.kasuladevi@ge.com>
2018-04-06 14:07:22 +0530

    correction

--- src/components/Option.js
+++ src/components/Option.js
@@ -3,13 +3,11 @@ import React from 'react';
 // stateless Option Component
 export const Option = (props) => {
 	return (
-		<ul>
-			<li> 
-				{props.optionText} 
-				<button onClick={ (e) => {
-					props.handleDeleteOption(props.optionText);
-				}}>remove</button>
-			</li>
-		</ul>
+		<li> 
+			{props.optionText} 
+			<button onClick={ (e) => {
+				props.handleDeleteOption(props.optionText);
+			}}>remove</button>
+		</li>
 	);
 };
--- src/components/Options.js
+++ src/components/Options.js
@@ -6,13 +6,15 @@ export const Options = (props) => {
 	return (
 		<div>
 			{ props.options.length === 0 && <p>Add an option to get started.</p>}
-			{ props.options.map((option) => (
-				<Option 
-					key={option} 
-					optionText={option}
-					handleDeleteOption={props.handleDeleteOption}>
-				</Option>
-			) ) }
+			<ul>
+				{ props.options.map((option) => (
+					<Option 
+						key={option} 
+						optionText={option}
+						handleDeleteOption={props.handleDeleteOption}>
+					</Option>
+				) ) }
+			</ul>
 			<button onClick={props.handleDeleteOptions}>Remove All</button>
 		</div>
 	);
